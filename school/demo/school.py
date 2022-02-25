c8bb0276e178589b0fce6223d127c469488d43c2
Hinamizawa <themoonbunnys@gmail.com>
2022-02-25 10:21:42 -0400

    update

--- school/demo/school.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-from odoo.exceptions import UserError
-
-
-class SchoolProfile (models.Model):
-    _name = "school.profile"
-    _description = "Showing users profile"
