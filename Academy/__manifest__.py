fbcedbfa947fdf40b4ed9afd72135417b8eba33e
Hinamizawa <themoonbunnys@gmail.com>
2022-02-25 10:06:45 -0400

    update

---
+++ Academy/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo School',
+
+    'summary': """App to do schooling management """,
+
+    'description': """
+        A school management profile for the users
+    
+    """,
+    'author': 'Hinamizawa',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Profile Management',
+    'company': 'SMART-OBC',
+
+    'version': '14.0.0',
+
+    'depends': ['base', 'Odoo School'],
+
+    'data': [
+
+
+    ],
+    'demo': [
+
+
+    ],
+
+
+
+
+}
